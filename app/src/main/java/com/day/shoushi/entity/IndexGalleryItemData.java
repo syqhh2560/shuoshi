c31434060de94d5010047c7c97b544bca8a104dc
syqhh2560 <260774416@qq.com>
2017-04-25 20:34:58 +0800

    实体类

---
+++ app/src/main/java/com/day/shoushi/entity/IndexGalleryItemData.java
@@ -0,0 +1,38 @@
+package com.day.shoushi.entity;
+
+/**
+ * Created by syqhh2560 on 2017/4/25.
+ */
+
+public class IndexGalleryItemData {
+
+        private int id;
+        private String imageUrl;
+        private String price;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+    }
+
